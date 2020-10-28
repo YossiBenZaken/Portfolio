955a2935fb389c646fa541129b165f7d516b3807
יוסי בן זקן <yossib@ordaprint.local>
2020-10-28 15:46:31 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Portfolio';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const script = document.createElement('script');
+    script.src = 'assets/js/scripts.js';
+    setTimeout(() => document.body.appendChild(script), 2000);
+  }
 }
